48e8dee116f0af52e4b1a160fa5f0f6864bde097
Bergen Dean <bddean21@email.unc.edu>
2023-11-05 19:18:26 -0500

    create equipment reservation component

---
+++ frontend/src/app/equipment-reservation/equipment-reservation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equipment-reservation',
+  templateUrl: './equipment-reservation.component.html',
+  styleUrls: ['./equipment-reservation.component.css']
+})
+export class EquipmentReservationComponent {
+
+}
