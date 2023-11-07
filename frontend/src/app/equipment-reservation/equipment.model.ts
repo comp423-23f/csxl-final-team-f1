fbaa37308be08fc3f86d84b3142762457f93192e
Alan Van Dang <alandang9213@gmail.com>
2023-11-06 19:08:54 -0500

    Add equipment files to corresponding organization files

---
+++ frontend/src/app/equipment-reservation/equipment.model.ts
@@ -0,0 +1,9 @@
+import { Event } from '../event/event.model';
+
+/** Interface for Equipment Type (used on frontend for equipment detail) */
+export interface Equipment {
+  id: number | null;
+  name: string;
+  logo: string;
+  short_description: string;
+}
